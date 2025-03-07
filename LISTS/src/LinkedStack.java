a49575f5bd5ee5ff9b0da117f3d29e900255dbe8
KoshelevVA <koshelevva96@gmail.com>
2025-03-07 18:36:47 +0400

    done LISTS

--- LISTS/src/LinkedStack.java
+++ LISTS/src/LinkedStack.java
@@ -1,36 +1,51 @@
 public class LinkedStack {
-    private Node tail; // ссылка на последний добавленный узел (обёртку)
-    private int size; // размер стека, т.е. количество элементов в нём
+    private Node tail;
+    private int size;
 
     public void push(int value) {
-        Node node = new Node(value); // создаём новый узел
-        if (tail != null) { // если в стеке уже есть элементы
-            node.setPrev(tail); // связываем новый узел с последним
+        Node node = new Node(value);
+        if (tail != null) {
+            node.setPrev(tail);
         }
-        tail = node; // назначаем новый узел последним узлом
-        size++; // увеличиваем счётчик элементов
+        tail = node;
+        size++;
     }
 
     public int pop() {
-        // ваш код
-        // возьмите value из последнего узла
-        // назначьте предыдущий к последнему узлу последним узлом
+        if (this.isEmpty()) {
+            return -1;
+        }
+        int result = tail.getValue();
+        tail = tail.getPrev();
+        size--;
+        return result;
     }
 
     public int getSize() {
-        // ваш код
-        // верните размер стека
+        return size;
     }
 
     public boolean isEmpty() {
-        // ваш код
-        // верните ответ на вопрос, не пустой ли стек
+        return size == 0;
     }
 
+    @Override
     public String toString() {
-        // если есть элементы, пройдитесь по связному списку,
-        // выводя элементы.
-        // вывод должен быть в точности как в комментариях к main
-        // при этом этот метод не должен менять стек!
+        StringBuilder sb = new StringBuilder();
+        if (!this.isEmpty()) {
+            Node currentNode = tail;
+            int i = size;
+            while (i != 0) {
+                sb.append(currentNode.getValue()).append(" ");
+                if (currentNode.getPrev() != null) {
+                    sb.append("->");
+                }
+                currentNode = currentNode.getPrev();
+                i--;
+            }
+        } else {
+            return "EMPTY";
+        }
+        return sb.toString();
     }
 }
